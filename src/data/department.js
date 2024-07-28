954f70e6096b41abc189580814ed1a8ae6471cbd
Paul Bonoso <pbonoso@drv.es>
2024-07-28 11:34:08 +0200

    third commit

---
+++ src/data/department.js
@@ -0,0 +1,7 @@
+export const departmentsList = [
+    { label: 'Sales', value: 'Sales' },
+    { label: 'Marketing', value: 'Marketing' },
+    { label: 'Engineering', value: 'Engineering' },
+    { label: 'Human Resources', value: 'Human Resources' },
+    { label: 'Legal', value: 'Legal' },
+]
